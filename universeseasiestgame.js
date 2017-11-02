98e9bcff77f2a2fe949662de0f535c5d91b2b221
21jamalg <32182788+21jamalg@users.noreply.github.com>
2017-11-02 16:59:21 -0700

    Update universeseasiestgame.js

--- universeseasiestgame.js
+++ universeseasiestgame.js
@@ -1,1 +1,9 @@
-document.body.style.backgroundColor = "red";
+fnvnfnvfjnf
+fvf mv fmv mf v fmv f
+vfvfvjfv fj vjf
+fjvjfjv fvf
+ f f vfm fmv
+ f vfj vjf  jfvv
+ f vf vfn f  f
+ vfkmvfvnkfnfnknvfkn
+ fvjvnfjvjfvnjv
