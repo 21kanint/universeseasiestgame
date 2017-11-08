5f1e13b3c951894583587d985976576294742a81
nikpedersen <33270351+nikpedersen@users.noreply.github.com>
2017-11-07 16:51:00 -0800

    Update universeseasiestgame.js

--- universeseasiestgame.js
+++ universeseasiestgame.js
@@ -2,11 +2,42 @@
 let canvas = document.getElementById("grid");
 let ctx = canvas.getContext("2d")
 
-rectx = rect(15, 15, 15, 15);
-recty = rect(15, 15, 15, 15);
-
 let player = {
  isAlive: true,
- x: rectx,
- y: recty
+ x: 5,
+ y: 5,
 };
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#8CE8FF"
+  ctx.fillRect(player.x, player.y, 20, 20);
+  requestAnimationFrame(draw);
+};
+
+
+
+
+document.addEventListener('keydown', function(event) {
+//left move    
+  if(event.keyCode == 37) {
+        player.x -= 8;
+    }
+  
+  //right move
+    else if(event.keyCode == 39) {       
+      player.x += 8;
+
+    }
+  
+  //up move
+    else if(event.keyCode == 38) {
+      player.y -= 8;
+    }
+  
+  //down move
+  else if(event.keyCode == 40) {
+    player.y += 8;
+  }});
+
+draw();
