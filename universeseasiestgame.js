fb068c72b6c36a9630e22cbddcf3c54994c97194
nikpedersen <33270351+nikpedersen@users.noreply.github.com>
2017-11-09 17:53:55 -0800

    Update universeseasiestgame.js

--- universeseasiestgame.js
+++ universeseasiestgame.js
@@ -1,8 +1,14 @@
 //calling the canvas
 let canvas = document.getElementById("grid");
 let ctx = canvas.getContext("2d")
+
+let playerspd = 1;
+
 let kbd = {
   u: false,
+  d: false,
+  l: false,
+  r: false
 };
 
 let enemies = {
@@ -13,39 +19,82 @@ let player = {
  isAlive: true,
  x: 5,
  y: 5,
- draw : function() {
+  move: function(direction) {
+    if(direction === "u") {
+      player.y -= playerspd;
+    }
+
+    else if(direction === "d") {
+      player.y += playerspd;
+    }
+
+    else if(direction === "l") {
+      player.x -= playerspd
+    }
+
+    else if(direction === "r") {
+      player.x += playerspd
+    }
+  },
+  
+  draw: function() {
   ctx.fillStyle = "#8CE8FF";
   ctx.fillRect(this.x, this.y, 20, 20);
  }
 };
 
-
 function init() {
   document.addEventListener('keydown', function(e) {
 //left move    
   if(e.keyCode === 37) {
-        player.x -= 8;
+        kbd.l = true;
         e.preventDefault();
     }
   
   //right move
     else if(e.keyCode === 39) {       
-      player.x += 8;
+      kbd.r = true;
       e.preventDefault();
     }
   
   //up move
     else if(e.keyCode === 38) {
-      player.y -= 8;
+      kbd.u = true;
       e.preventDefault();
     }
   
   //down move
   else if(e.keyCode === 40) {
-    player.y += 8;
+    kbd.d = true;
     e.preventDefault();
   }
 });
+
+  document.addEventListener('keyup', function(e) {
+//left move    
+  if(e.keyCode === 37) {
+        kbd.l = false;
+        e.preventDefault();
+    }
+  
+  //right move
+    else if(e.keyCode === 39) {       
+      kbd.r = false;
+      e.preventDefault();
+    }
+  
+  //up move
+    else if(e.keyCode === 38) {
+      kbd.u = false;
+      e.preventDefault();
+    }
+
+    //down move
+    else if(e.keyCode === 40) {
+      kbd.d = false;
+      e.preventDefault();
+    }
+  });
 
   
   update();
@@ -53,17 +102,21 @@ function init() {
 
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if(kbd.u) {
+    player.move("u");
+  } 
+  if(kbd.d)  {
+    player.move("d");
+  } 
+  if(kbd.l) {
+    player.move("l");
+  }
+  if(kbd.r) {
+    player.move("r");
+  }
   
   player.draw();
-  
-  
   requestAnimationFrame(update);
 }
-
-  
-
-
-
-
 
 init();
